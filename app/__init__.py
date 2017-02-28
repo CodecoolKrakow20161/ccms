4fba8beeff34f50dc36688ed299d6a116cf6e98e
Przemysław <przemyslaw.ciacka@gmail.com>
2017-02-28 16:30:22 +0100

    Default 404 handler and template

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+
+# Sample HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
