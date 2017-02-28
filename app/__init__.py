0e77e78ecb003ddbc6d876c593fdbd5b4ff7d7ad
Przemysław <przemyslaw.ciacka@gmail.com>
2017-02-28 16:44:50 +0100

    Register mod_people blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,7 @@ app.config.from_object('config')
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+from app.mod_people.controllers import mod_people
+
+app.register_blueprint(mod_people)
