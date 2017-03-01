bf64077f8a28f85343fcf37cc922373ad32c93dc
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-01 10:11:10 +0100

    Configure app to use SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Configurations
 app.config.from_object('config')
+
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
 
 # Sample HTTP error handling
 @app.errorhandler(404)
@@ -13,3 +18,7 @@ def not_found(error):
 from app.mod_default.controllers import mod_default
 
 app.register_blueprint(mod_default)
+
+# Build the database:
+# This will create the database file using SQLAlchemy
+db.create_all()
