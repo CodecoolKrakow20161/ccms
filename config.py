1f726188237585ac7e91f43969b0feae85b667d2
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-01 10:04:48 +0100

    Add sqlalchemy config

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ DEBUG = True
 # Define the application directory
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+DATABASE_CONNECT_OPTIONS = {}
