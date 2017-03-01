0abc87bfb1662cd80d363b7a38b29360691a6aed
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-01 11:44:32 +0100

    Configure CSRF

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # SQLite for this example
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 DATABASE_CONNECT_OPTIONS = {}
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED     = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = "secret"
