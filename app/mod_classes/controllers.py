9de2ea62a0e836a99a5e059d8bc99f9d727c1d48
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-01 11:35:42 +0100

    List of classes

--- app/mod_classes/controllers.py
+++ app/mod_classes/controllers.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from app import db
+from app.mod_classes.models import Klass
 
 # Define the blueprint: 'people', set its url prefix: app.url/people
 mod_classes = Blueprint('classes', __name__, url_prefix='/classes')
@@ -6,4 +8,5 @@ mod_classes = Blueprint('classes', __name__, url_prefix='/classes')
 # Set the route and accepted methods
 @mod_classes.route('/', methods=['GET'])
 def index():
-    return render_template('classes/index.html')
+    klasses = Klass.query.all()
+    return render_template('classes/index.html', klasses=klasses)
