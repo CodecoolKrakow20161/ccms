5f707909f7f455e9d96b84d25b399bfac3b696a3
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-08 15:28:41 +0100

    Change BaseModel timestamps column names

--- app/base_model.py
+++ app/base_model.py
@@ -1,9 +1,10 @@
 from app import db
 
+
 class BaseModel(db.Model):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                           onupdate=db.func.current_timestamp())
