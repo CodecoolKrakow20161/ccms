46b490375b20ebb8009761ab94a425a00c81aefb
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-09 20:49:45 +0100

    Disable SQLAlchemy track modifications

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ DEBUG = True
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+# SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and
+# will be disabled by default in the future
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # Define the database - we are working with
 # SQLite for this example
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
