e9b4e730f87559f4f486d693d1b4d43010f3c422
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-09 23:28:54 +0100

    Add application version

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from app.__version__ import version
 
 app = Flask(__name__)
 
@@ -14,6 +15,11 @@ db = SQLAlchemy(app)
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+# Add app_version variable to Jinja2 templates context
+@app.context_processor
+def app_version():
+    return dict(app_version=version)
 
 from app.mod_default.controllers import mod_default
 from app.mod_classes.controllers import mod_classes
