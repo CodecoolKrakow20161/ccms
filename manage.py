da00423242fe474e3ab7dcd862ed313ac08b4d4d
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-09 21:54:10 +0100

    User Migrate in order to create migrations

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 from flask_script import Manager
-from flask_migrate import MigrateCommand
-from app import app
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     manager.run()
