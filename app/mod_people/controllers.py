9f39341e07987f3835bb90d7bc633b6736ad1565
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-11 23:56:27 +0100

    Add mod_people:index method with base template

--- app/mod_people/controllers.py
+++ app/mod_people/controllers.py
@@ -2,3 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 # Define the blueprint: 'people', set its url prefix: app.url/people
 mod_people = Blueprint('people', __name__, url_prefix='/people')
+
+@mod_people.route('/')
+def index():
+    return render_template('people/index.html')
