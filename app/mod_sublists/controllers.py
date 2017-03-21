8ffa22a35ab365a7a3827c2eeed9fc5bc00d2645
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-21 17:06:07 +0100

    Add edit action of sublists resource

--- app/mod_sublists/controllers.py
+++ app/mod_sublists/controllers.py
@@ -21,6 +21,18 @@ def new():
 
     return render_template('sublists/new.html', form=form)
 
+@mod_sublists.route('/<int:id>/edit', methods=['GET', 'POST'])
+def edit(id):
+    sublist = Sublist.query.get_or_404(id)
+    form = SublistForm(request.form, obj=sublist)
+    if request.method == "POST" and form.validate():
+        form.populate_obj(sublist)
+        db.session.commit()
+
+        return redirect(url_for('sublists.index'))
+
+    return render_template('sublists/edit.html', form=form, sublist=sublist)
+
 @mod_sublists.route('/<int:id>/delete', methods=['POST'])
 def delete(id):
     sublist = Sublist.query.get_or_404(id)
