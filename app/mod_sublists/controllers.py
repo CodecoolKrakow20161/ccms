07671ce5b731485f2e3fc8d6a65813d875eb4417
Przemysław <przemyslaw.ciacka@gmail.com>
2017-03-21 14:39:22 +0100

    GET /sublists route with base template

--- app/mod_sublists/controllers.py
+++ app/mod_sublists/controllers.py
@@ -2,3 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from app import db
 
 mod_sublists = Blueprint('sublists', __name__, url_prefix='/sublists')
+
+@mod_sublists.route('/')
+def index():
+    return render_template('sublists/index.html')
